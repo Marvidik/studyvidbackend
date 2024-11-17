ffb79a9ebe29b2508763253d54f596aad2909628
unknown <ebubeidika@gmail.com>
2024-11-17 01:32:49 +0100

    google auth finally works

--- users/urls.py
+++ users/urls.py
@@ -6,9 +6,8 @@ from .views import GoogleLogin,GoogleLoginCallback,GoogleLoginRedirect
 urlpatterns = [
     path("login/", login, name="login"),
     path("signup/", register, name="register"),
-    # path("add-profile/",add_profile,name="profileadd"),
-    # path("profile/",profile,name="profile")
-    
+   
+    #social auth urls
     path("api/v1/auth/", include("dj_rest_auth.urls")),
     re_path(r"^api/v1/auth/accounts/", include("allauth.urls")),
     path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
